75347842d83eedbfaa1bfa50b462826cf11b9cd4
Abdul Rafay <rafayjutt851@gmail.com>
2026-03-25 23:34:32 +0500

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main(){
+   char operator;
+   double first, second;
+   printf("Enter an operator (+ , - , * , /): ");
+   scanf(" %c", &operator);
+   printf("Enter two numbers: ");
+   scanf("%lf %lf", &first, &second);
+   switch (operator)
+   {
+   case '+':
+    printf("%.2f + %.2f = %.2f\n",first , second , first + second);
+    break;
+   case '-':
+    printf("%.2f - %.2f = %.2f\n",first , second , first - second);
+    break;
+   case '*':
+    printf("%.2f * %.2f = %.2f\n",first , second , first * second);
+    break;
+    case '/':
+    if(second !=0.0){
+    printf("%.2f / %.2f = %.2f\n",first , second , first / second);
+    }
+    else{
+        printf("Error! Division by zero is not allowed.\n");
+    }
+    break;
+   default:
+    printf("Error! Operator is not correct.\n");
+    break;
+   }
+    return 0;
+}
